1712ef894459289836ff3c895e0e8c479515da13
unknown <yatenbueno@gmail.com>
2025-12-07 19:57:59 -0300

    refactor: implementar lógica por si no hay coincidencias con los filtros

--- src/components/ContenidoList.tsx
+++ src/components/ContenidoList.tsx
@@ -2,8 +2,10 @@ import { AudiovisualesContext } from "@/src/context/audiovisual-context";
 import { ROUTES } from "@/src/navigation/routes";
 import { Link } from "expo-router";
 import { useContext } from "react";
-import { FlatList, StyleSheet } from "react-native";
+import { FlatList, StyleSheet, View } from "react-native";
 import { ContenidoCard } from "./ContenidoCard";
+import { Texto } from "../common/constants/FuenteProvider";
+import colors from "../common/constants/Colors";
 
 type ContenidoListProps = {
   tipoId: number;
@@ -27,6 +29,17 @@ export function ContenidoList({
 
     return coincideTipo && coincideGenero;
   });
+
+  // Renderizado condicional si no hay resultados
+  if (filtrado.length === 0) {
+    return (
+      <View style={styles.emptyContainer}>
+        <Texto style={styles.emptyText}>
+          No se encontraron coincidencias para los filtros aplicados.
+        </Texto>
+      </View>
+    );
+  }
 
   return (
     <FlatList
@@ -52,5 +65,17 @@ export function ContenidoList({
 const styles = StyleSheet.create({
   contentContainer: {
     gap: 10,
+  },
+  emptyContainer: {
+    height: 100, // Altura mínima para que la caja no colapse
+    justifyContent: "center",
+    alignItems: "center",
+    opacity: 0.7,
+  },
+  emptyText: {
+    color: colors.blanco,
+    fontSize: 10,
+    textAlign: "center",
+    maxWidth: "80%", // Para que no toque los bordes si es texto largo
   },
 });
